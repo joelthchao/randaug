8c94746a98842ac50b4746e03750c49f9a701e8b
Joel Chao <joel.chao@appier.com>
2017-04-15 01:02:13 +0800

    all conv net

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, GlobalAveragePooling2D
 from keras.models import Model
 
 
@@ -11,9 +11,14 @@ def build_simple_model(input_shape, num_classes):
     net = Conv2D(192, (3, 3), padding='same', activation='relu') (net)
     net = Conv2D(192, (3, 3), padding='same', activation='relu') (net)
     net = MaxPooling2D((3, 3), strides=2)(net)
-    flatten = Flatten() (net)
 
-    net = Dense(512, activation='relu') (flatten)
-    out = Dense(num_classes, activation='softmax') (net)
+    net = Conv2D(192, (3, 3), padding='same', activation='relu') (net)
+    net = Conv2D(192, (1, 1), padding='same', activation='relu') (net)
+    net = Conv2D(10, (1, 1), padding='same', activation='relu') (net)
+
+    net = GlobalAveragePooling2D() (net)
+
+    flatten = Flatten() (net)
+    out = Dense(num_classes, activation='softmax') (flatten)
 
     return Model(input, out)
