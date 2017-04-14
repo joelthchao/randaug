73a0a4bea123b5e8611aa6e83452c809eef27581
Joel Chao <joel.chao@appier.com>
2017-04-15 00:42:50 +0800

    fix data issue

--- aux_disc_loss.py
+++ aux_disc_loss.py
@@ -1,10 +1,30 @@
 import argparse
 import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import keras
 from keras.datasets import cifar10
 
 from model import build_simple_model
+
+
+def get_cifar10_data():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # Convert class vectors to binary class matrices.
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
+    return x_train, y_train, x_test, y_test
 
 
 def main():
@@ -18,14 +38,7 @@ def main():
     num_classes = 10
     epochs = 200
 
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-
-    # Convert class vectors to binary class matrices.
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+    x_train, y_train, x_test, y_test = get_cifar10_data()
 
     model = build_simple_model(x_train.shape[1:], num_classes)
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
