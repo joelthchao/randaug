572a58dd1d5d319093ea41e0c71286194bf4cb4c
Joel Chao <joel.chao@appier.com>
2017-04-16 21:59:33 +0800

    sgd style training

--- aux_disc_loss.py
+++ aux_disc_loss.py
@@ -7,8 +7,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import keras
 from keras.datasets import cifar10
-from keras.optimizers import Adam, RMSprop
-from keras.callbacks import TensorBoard
+from keras.optimizers import Adam, RMSprop, SGD
+from keras.callbacks import Callback, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
 
 from model import build_simple_model
@@ -54,6 +54,46 @@ def run_simple_model(log_dir, batch_size=32, optimizer=Adam, lr=0.0001, epochs=2
               validation_data=(x_test, y_test), callbacks=callbacks)
 
 
+def run_sgd(log_dir, batch_size=128, lr=0.1, epochs=200, data_augmentation=False):
+    num_classes = 10
+
+    log_name = 'wide_resnet_bs_{}_op_{}_lr_manual_ep_{}_ag_{}'.format(
+            batch_size, SGD.__name__, epochs, data_augmentation)
+    log_path = os.path.join(log_dir, log_name)
+
+    x_train, y_train, x_test, y_test = get_cifar10_data(num_classes)
+
+    model = build_wide_resnet_model(x_train.shape[1:], num_classes)
+    optimizer = SGD(lr, momentum=0.9, nesterov=True)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    class ManualSGDLRCallback(Callback):
+        def __init__(self, lr_schedule):
+            self.lr_schedule = lr_schedule
+            super(Callback, self).__init__()
+
+        def on_epoch_begin(self, epoch, logs=None):
+            if epoch in self.lr_schedule:
+                self.model.optimizer.lr = self.lr_schedule[epoch]
+
+    callbacks = [TensorBoard(log_path), ManualSGDLRCallback({0:0.1, 80: 0.01, 120: 0.001})]
+
+    if data_augmentation:
+        img_gen = ImageDataGenerator(
+                width_shift_range=0.125,
+                height_shift_range=0.125,
+                fill_mode='reflect',
+                horizontal_flip=True
+        )
+        train_data_gen = img_gen.flow(x_train, y_train, batch_size=batch_size)
+        steps_per_epoch = (x_train.shape[0] - 1) // batch_size + 1
+        model.fit_generator(train_data_gen, steps_per_epoch=steps_per_epoch,
+                            verbose=1, epochs=epochs, validation_data=(x_test, y_test), callbacks=callbacks)
+    else:
+        model.fit(x_train, y_train, batch_size=batch_size, verbose=1, epochs=epochs,
+                  validation_data=(x_test, y_test), callbacks=callbacks)
+
+
 def run(log_dir, batch_size=128, optimizer=Adam, lr=0.001, epochs=20, data_augmentation=False):
     num_classes = 10
 
@@ -91,7 +131,7 @@ def main():
 
     args = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
-    run(args.log_dir, epochs=20, data_augmentation=True)
+    run_sgd(args.log_dir, epochs=200, data_augmentation=True)
 
 
 if __name__ == '__main__':
